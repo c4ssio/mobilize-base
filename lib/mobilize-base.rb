5dbda1da20efce278e48e589ba258fff861b8c69
c4ssio <cassio.paesleme@gmail.com>
2014-02-04 16:53:24 -0500

    amending to match working config on new host

--- lib/mobilize-base.rb
+++ lib/mobilize-base.rb
@@ -23,27 +23,18 @@ module Mobilize
     def Base.config_dir
       ENV['MOBILIZE_CONFIG_DIR'] ||= "config/mobilize/"
     end
-    def Base.config_file(config_name)
+    def Base.config(config_name)
       config_dir = begin
                      "#{::Rails.root}/#{Base.config_dir}"
                    rescue
                      "#{Base.root}/#{Base.config_dir}"
                    end
-      file = "#{config_dir}#{config_name}.yml"
-      raise "Could not find #{file}" unless ::File.exists?(file)
-      file
-    end
-    def Base.config(config_name)
-      @config ||= {}
-      if !@config[config_name]
-        yaml_path = Base.config_file(config_name)
-        if ::File.exists?(yaml_path)
-          @config[config_name] = ::YAML.load_file(yaml_path)[Base.env]
-        else
-          raise "Could not find #{yaml_path}"
-        end
+      yaml_path = "#{config_dir}#{config_name}.yml"
+      if ::File.exists?(yaml_path)
+        return ::YAML.load_file(yaml_path)[Base.env]
+      else
+        raise "Could not find #{config_name}.yml in #{config_dir}"
       end
-      @config[config_name]
     end
     def Base.env
       begin
@@ -93,7 +84,7 @@ require 'google_drive'
 require 'resque'
 require "mobilize-base/handlers/resque"
 #specify appropriate redis port per resque.yml
-Resque.redis = "127.0.0.1:#{Mobilize::Resque.config['redis_port']}"
+Resque.redis = "#{Mobilize::Resque.config['redis_host']}:#{Mobilize::Resque.config['redis_port']}"
 require 'popen4'
 require "mobilize-base/jobtracker"
 require "mobilize-base/handlers/google/gdrive"
